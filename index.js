2cbdd189183754a7ef7daed7dbb91f5033c6886d
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-18 15:02:02 -0600

    Add renderParties function to display in the html

--- index.js
+++ index.js
@@ -1,26 +1,17 @@
 const COHORT = "2408-RDERIEUX";
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/recipes/`;
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/events/`;
 
 // === State ===
 const state = {
-  parties: [
-    {
-      id: 1,
-      name: "Event Name",
-      description: "This is a description of the event.",
-      date: "2021-09-30T00:00:00.000Z", // Date ISO string
-      location: "123 Street",
-    },
-  ],
+  parties: [],
 };
 
 // Get all the parties from the API
 async function getParties() {
-  // TODO
   try {
     const response = await fetch(API_URL);
     const responseObj = await response.json();
-    debugger;
+
     state.parties = responseObj.data;
   } catch (error) {
     console.error(error);
@@ -37,6 +28,32 @@ async function addParty() {
 // Render all the parties
 function renderParties() {
   // TODO
+  const partyList = document.querySelector("#parties");
+
+  if (!state.parties.length) {
+    partyList.innerHTML = "<li>No parties for you!</li>";
+    return;
+  }
+
+  const partyCards = state.parties.map((party) => {
+    const card = document.createElement("li");
+    card.innerHTML = `
+            <dt>${party.name}</dt>
+            <dd>${party.date}</dd>
+            <dd>${party.location}</dd>
+            <dd>${party.description}</dd>
+        `;
+    return card;
+  });
+
+  partyList.replaceChildren(...partyCards);
+}
+
+//Sync the state with the API and rerender
+async function render() {
+  await getParties();
+  renderParties();
 }
 
 // === Script ===
+render();
