f651c8134acae678ccb1ed3263ee18355863c3ca
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-18 14:37:38 -0600

    Setup bare bones JS

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
 const COHORT = "2408-RDERIEUX";
-const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/recipes/
+const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/recipes/`;
 
-`;
+// === State ===
+const state = {
+  parties: [],
+};
+
+// Get all the parties
+async function getParties() {
+  // TODO
+}
+
+// Add a party
+async function addParty() {
+  // TODO
+}
+
+// === Render ===
+
+// Render all the parties
+function renderParties() {
+  // TODO
+}
+
+// === Script ===
