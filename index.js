7cc34ca1f5768272f1d816c614247278ca8efbc5
Robert DeRieux <ppldiecomplex@gmail.com>
2024-09-18 14:45:23 -0600

    Add the getParties function to retrieve all parties from the API

--- index.js
+++ index.js
@@ -3,12 +3,28 @@ const API_URL = `https://fsa-crud-2aa9294fe819.herokuapp.com/api/${COHORT}/recip
 
 // === State ===
 const state = {
-  parties: [],
+  parties: [
+    {
+      id: 1,
+      name: "Event Name",
+      description: "This is a description of the event.",
+      date: "2021-09-30T00:00:00.000Z", // Date ISO string
+      location: "123 Street",
+    },
+  ],
 };
 
-// Get all the parties
+// Get all the parties from the API
 async function getParties() {
   // TODO
+  try {
+    const response = await fetch(API_URL);
+    const responseObj = await response.json();
+    debugger;
+    state.parties = responseObj.data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 // Add a party
